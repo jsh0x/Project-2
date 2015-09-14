af2f034aa73d21d58071c0cbf009a9ce65356fe9
jsh0x <jshox.zap@gmail.com>
2015-09-14 08:47:43 -0600

    Added additional methods from the edmunds API

--- edmunds.py
+++ edmunds.py
@@ -19,3 +19,54 @@ class Edmunds:
 
         return retval
 
+
+
+    def get_models(self):
+        retval = {}
+        makeCount = 0
+        modelCount = 0
+        modelList = []
+
+        payload = {'fmt': 'json', 'api_key': self.api_key}
+        r = requests.get('https://api.edmunds.com/api/vehicle/v2/makes', params=payload)
+        json_object = json.loads(r.text)
+
+        for make in json_object['makes']:
+            for model in json_object['makes'][makeCount]['models']:
+                modelList.append(json_object['makes'][makeCount]['models'][modelCount]['name'])
+                modelCount += 1
+            retval[make['name']] = modelList
+            makeCount += 1
+            modelCount = 0
+            modelList = []
+
+        return retval
+
+    def get_years(self):
+        retval = {}
+        makeCount = 0
+        modelCount = 0
+        yearCount = 0
+        modelList = []
+        yearList = []
+
+        payload = {'fmt': 'json', 'api_key': self.api_key}
+        r = requests.get('https://api.edmunds.com/api/vehicle/v2/makes', params=payload)
+        json_object = json.loads(r.text)
+"""
+        for make in json_object['makes']:
+            for model in json_object['makes'][makeCount]['models']:
+                for year in json_object['makes'][makeCount]['models'][modelCount]['years']:
+                    yearList.append(json_object['makes'][makeCount]['models'][modelCount]['years'][yearCount]['year'])
+                    yearCount += 1
+                modelList.append(json_object['makes'][makeCount]['models'][modelCount]['name'])
+                modelList.append(yearList)
+                modelCount += 1
+                yearCount = 0
+                yearList = []
+            retval[make['name']] = modelList
+            makeCount += 1
+            modelCount = 0
+
+        return retval
+"""
